7e016cce79eaadbd420c873dbb787b8a8d1381e2
James McClanahan <jim30109@gmail.com>
2022-08-14 16:15:00 -0400

    Update install.sh
    Must use SU permissions to put in /usr/local/bin

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@
 #
 gcc -I. -o a18 a18.c a18util.c a18eval.c
 rm -rf /usr/local/bin/a18 || true
-ln -s $HOME/Software/A18_Assembler/a18 /usr/local/bin/a18
+sudo ln -s $HOME/Software/A18_Assembler/a18 /usr/local/bin/a18
 chmod +x a18
