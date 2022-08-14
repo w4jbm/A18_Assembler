17a2a188d77ec8ed55f83fb20e8963591d2444aa
James McClanahan <jim30109@gmail.com>
2022-08-14 16:06:30 -0400

    Update install.sh
    Changes for MX Linux which does not have ~/.local/bin in PATH.

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 # Simple install script for A18
 #
 gcc -I. -o a18 a18.c a18util.c a18eval.c
-rm -rf /home/jbm/.local/bin/a18 || true
-ln -s $HOME/Software/A18_Assembler/a18 $HOME/.local/bin/a18
+rm -rf /usr/local/bin/a18 || true
+ln -s $HOME/Software/A18_Assembler/a18 /usr/local/bin/a18
 chmod +x a18
-
